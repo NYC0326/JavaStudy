243ee575631508c1ca1186d5febc341ae7ce3487
남유찬 <bryan0326@naver.com>
2023-04-23 22:34:38 +0900

    DelemiterDemo RE

--- src/Chap2Lab_RE/DelemiterDemo.java
+++ src/Chap2Lab_RE/DelemiterDemo.java
@@ -1,5 +1,29 @@
 package Chap2Lab_RE;
 
+import java.util.Scanner;
+
 public class DelemiterDemo {
-	
+	public static void main(String[] args) {
+		// word1 word2 junk
+		Scanner keyboard1 = new Scanner(System.in);
+		Scanner keyboard2 = new Scanner(System.in);
+		keyboard2.useDelimiter("##");
+
+		System.out.println("Enter a line of text:");
+		String word1 = keyboard1.next();
+		String word2 = keyboard1.next();
+		
+		System.out.println("For keyboard1 the two words read are:");
+		System.out.println(word1);
+		System.out.println(word2);
+		String junk = keyboard1.nextLine();
+
+		System.out.println("Reenter the same line of text:");
+		word1 = keyboard2.next();
+		word2 = keyboard2.next();
+
+		System.out.println("For keyboard2 the words read are:");
+		System.out.println(word1);
+		System.out.println(word2);
+	}
 }
