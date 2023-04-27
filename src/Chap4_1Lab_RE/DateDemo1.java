4f78f28cefd46762f2806628ca04deb5e58c5bb2
남유찬 <bryan0326@naver.com>
2023-04-27 11:25:59 +0900

    Date RE, toString() 메소드의 이해

---
+++ src/Chap4_1Lab_RE/DateDemo1.java
@@ -0,0 +1,19 @@
+package Chap4_1Lab_RE;
+
+public class DateDemo1 {
+	public static void main(String[] args) {
+		Date date1 = new Date(), date2 = new Date();
+		date1.readInput();
+		date2.setDate(8, 15, 1945);
+
+		if (date1.equals(date2))
+			System.out.println(date1 + " equals " + date2);
+		else
+			System.out.println(date1 + " does not equal " + date2);
+		
+		if (date2.precedes(date1))
+			System.out.println(date2 + " comes before " + date1);
+		else
+			System.out.println(date2 + " does not comes before " + date1);
+	}
+}
